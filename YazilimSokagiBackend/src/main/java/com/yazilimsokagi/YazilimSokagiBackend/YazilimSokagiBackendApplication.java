70bdffcc9b39d1b307843e67bf743bfee44f019f
Adem Demir <93316402+AdemDemirFE@users.noreply.github.com>
2024-11-09 00:57:05 +0300

    Java SpringBoot Backend Project Created with Java 17 and Spring Boot 3.3.5 version

---
+++ YazilimSokagiBackend/src/main/java/com/yazilimsokagi/YazilimSokagiBackend/YazilimSokagiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yazilimsokagi.YazilimSokagiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YazilimSokagiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YazilimSokagiBackendApplication.class, args);
+	}
+
+}
---
+++ YazilimSokagiBackend/src/test/java/com/yazilimsokagi/YazilimSokagiBackend/YazilimSokagiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazilimsokagi.YazilimSokagiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YazilimSokagiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
